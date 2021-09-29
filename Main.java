9100ea5e17368c3e0aad4edddfd7eb45ed0f5459
Vishal Gupta <vishalgupta@vishallaptop.lan>
2021-09-29 10:49:36 -0700

    Commit sample program to git repo

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import  java.lang.Thread.*;
+
+import static java.lang.Thread.sleep;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            try {
+                sleep(10000);
+                System.out.println("time is now->" + java.time.LocalDateTime.now());
+            }
+            catch(Exception threadInteruptedException)
+            {
+                //do nothing
+            }
+        }
+
+    }
+
+
+}
